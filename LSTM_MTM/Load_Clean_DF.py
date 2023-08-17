fcb5f99b995c367e1624353ec85cfac48d5cb296
Juan <j.valdes20@imperial.ac.uk>
2023-08-17 17:34:58 +0100

    LSTM_SMX_MTM Initial Commit Raw data ready to be imported and manipulated for LSTM

---
+++ LSTM_MTM/Load_Clean_DF.py
@@ -0,0 +1,43 @@
+# Loading, Cleaning Volume-Gamma-Nd dataframes
+import pandas as pd
+import Clean_CSV
+import os
+
+global_dir = '/Users/mfgmember/Documents/Juan_Static_Mixer/ML/LSTM_SMX/RawData'
+
+### Generate dataframe with Gamma and drop volume values
+def extract_GVol(elem):
+    csv_file = os.path.join(global_dir,f'{elem}_GVol.csv')
+    df = pd.read_csv(csv_file)
+    df = Clean_CSV.clean_csv(df,list(df.columns.values)[1:3])
+    return df
+
+### Generate dataframe with number of drops
+def extract_Nd(elem):
+    Nd_csv_file = os.path.join(global_dir,'Nd',f'{elem}_dnum_corr.csv')
+    df = pd.read_csv(Nd_csv_file)
+    label_list = list(df.columns.values)
+    df.rename(columns={label_list[0]: 'Ndrops'}, inplace = True)
+    df['Time'] = df.apply(lambda row: row.name*0.005,axis=1)
+    df = df[['Time','Ndrops']]
+    return df
+
+### Generate dataframe with interfacial area
+def extract_IA(elem):
+    Nd_csv_file = os.path.join(global_dir,'IA',f'{elem}_IA_corr.csv')
+    df = pd.read_csv(Nd_csv_file)
+    label_list = list(df.columns.values)
+    df.rename(columns={label_list[0]: 'IA'}, inplace = True)
+    df['Time'] = df.apply(lambda row: row.name*0.005,axis=1)
+    df = df[['Time','IA']]
+    return df
+
+### Generate dataframe with drop volumes
+def extract_Vol(elem):
+    csv_file = os.path.join(global_dir,f'{elem}_Vol.csv')
+    df = pd.read_csv(csv_file)
+    df = Clean_CSV.clean_csv(df,list(df.columns.values)[1:2])
+    return df
+
+
+
